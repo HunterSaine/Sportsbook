cf95b76d15ab0a48ecc74704e9119564bda65df2
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-08-08 21:12:45 -0400

    created api call and exported data to csv

--- .venv/Main.py
+++ .venv/Main.py
@@ -1,5 +1,22 @@
 # Description: Main file for the application
-def fetch_data():
+import numpy as np
+import pandas as pd
+import requests
+import json
+from keys import apikey
+def fetch_data(apikey):
     # Fetch data from the API
     # Return the data
-    pass
+    data = requests.get('https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?apiKey=e9be841a2b734322476b32827df51230&regions=us&markets=h2h,spreads&oddsFormat=american')
+    return data
+
+# data = fetch_data(apikey).json()
+# print(data)
+# df = pd.json_normalize(data)
+# print(df.head())
+# df.to_csv('data.csv', index=False)
+# df['bookmaker_titles'] = df['bookmakers'].apply(lambda x: [bookmaker['title'] for bookmaker in x])
+# filtered_df = df[['home_team', 'away_team', 'sport_title', 'commence_time', 'bookmaker_titles']]
+# # filtered_df.columns = ['Teams', 'Sites', 'Commence Time', 'H2H Odds', 'Spread Points', 'Spread Odds']
+# print(filtered_df.head())
+# filtered_df.to_csv('filtered_data.csv', index=False)
